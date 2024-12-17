df5d49685eb6fea66a8e7ebad89fd0128dcf857e
Eros Casas <ecasas182015@gmail.com>
2024-12-16 22:39:25 -0600

    Added turn signals for start & end turns

--- TurnManager.gd
+++ TurnManager.gd
@@ -8,6 +8,9 @@ var cur_char : Character
 
 var game_over : bool = false
 
+signal character_begin_turn(character)
+signal character_end_turn(character)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().create_timer(0.5).timeout
@@ -21,8 +24,14 @@ func begin_next_turn():
 		cur_char = player_char
 	else:
 		cur_char = player_char
+		
+#	emits a signal to start when it is a player turn or an enemy turn
+	emit_signal("character_begin_turn", cur_char)
 	
 func end_current_turn():
+	
+#	emits a signal to end turn when it is a player or enemy turn
+	emit_signal("character_end_turn", cur_char)
 	
 	await get_tree().create_timer(next_turn_delay).timeout
 	
